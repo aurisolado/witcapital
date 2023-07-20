6a349d2f33750fc58225d3f4732ebb2e842f2f90
aurisolado <oedwin.sh@gmail.com>
2023-07-20 13:24:05 -0600

    favicon
    favicon

--- .history/components/Meta_20230719235454.jsx
+++ .history/components/Meta_20230719235454.jsx
@@ -4,16 +4,8 @@ const Meta = ({ title, keyword, desc }) => {
   return (
     <div>
       <Head>
-        <title>{title} || Asesoría de vanguardia, con visión de negocios.</title>
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
-
-
+        <title>{title} || Slogal</title>
+        <link rel="icon" href="/favicon.png" />
         <meta name="description" content={desc} />
         <meta name="keyword" content={keyword} />
       </Head>
@@ -22,7 +14,7 @@ const Meta = ({ title, keyword, desc }) => {
 };
 
 Meta.defaultProps = {
-  title: "Wit Capital | Asesoría de vanguardia, con visión de negocios.",
+  title: "Wit Capital | Asesoría de vanguardia Next.js Template",
   keyword:
     "bitcoin, blockchain, crypto, crypto collectibles, crypto makretplace, cryptocurrency, digital items, market, nft, nft marketplace, nft next js, NFT react, non-fungible tokens, virtual asset, wallet",
   desc: "The world's first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.",
--- .history/components/hero/hero_bus_20230719235454.jsx
+++ .history/components/hero/hero_bus_20230719235454.jsx
@@ -20,10 +20,11 @@ const Hero_bus = () => {
 				<div className="container relative z-10">
 					<div className="mx-auto max-w-xl text-center">
 						<h1 className="mb-6 text-center font-display text-4xl font-medium text-jacarta-700 dark:text-white">
-                			Business  
+                Business  
 						</h1>
 						<p className="text-lg leading-normal dark:text-jacarta-200">
-							Contamos con las mejores herramientas para que tu negocio crezca de manera exponencial.
+							Be part of our young and innovative team, and we build trust, embrace feedback, grow
+							rapidly, and love our work.
 						</p>
 					</div>
 				</div>
