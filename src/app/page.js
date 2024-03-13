60a40e68f5d093390ddfad68f5103c2fcda44609
Edwin Hernandez <oedwin.sh@gmail.com>
2024-03-12 18:59:08 -0600

    Change title

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { HomeView } from 'src/sections/home/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimal: The starting point for your next project',
+  title: 'WIT BIZ - Asesoría de vanguardia, con visión de negocios',
 };
 
 export default function HomePage() {
